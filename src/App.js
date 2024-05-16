963bebc7e327c18c1fe37cc12b40faa6ec3e46a2
肖佳坤 <xiao@xiaojiakundeMacBook-Pro.local>
2024-05-16 21:48:37 +0800

    feat: init3

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className="SubappReact">
+      <header className="SubappReact-header">
         子应用react
       </header>
     </div>
